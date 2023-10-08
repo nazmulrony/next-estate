59d36983b281d23e253b6114dd940c53bde04356
nazmulrony <nhrony65@gmail.com>
2023-10-08 17:35:13 +0600

    database connected

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+	.connect(process.env.DB_URL)
+	.then(() => console.log('Database connected'))
+	.catch((error) => console.log(error));
 
 const app = express();
 
 const port = 4000;
 
 app.listen(port, () => {
-	console.log('Express server running at port 4000');
+	console.log(`Express server running at port ${port}`);
 });
