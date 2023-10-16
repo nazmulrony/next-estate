57fd2604f0290db4465aa44b048da215345747ee
nazmulrony <nhrony65@gmail.com>
2023-10-17 00:47:36 +0600

    sign in api started in auth.controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,9 +2,11 @@ import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 
+//sign up controller
 export const signup = async (req, res, next) => {
 	const { username, email, password } = req.body;
-	const hashedPassword = bcrypt.hashSync(password, 10);
+	const hashedPassword = bcrypt.hashSync(password, 10);// similar to await bcrypt.hash(password, 10)
+
 
 	const newUser = new User({ username, email, password: hashedPassword });
 
@@ -15,3 +17,24 @@ export const signup = async (req, res, next) => {
 		next(error);
 	}
 };
+
+//sign in controller
+export const signin = async (req, res, next) => {
+	const { email, password } = req.body
+
+	try {
+		//email check
+		const validUser = await User.findOne({ email })
+		if (!validUser) {
+			return next(errorHandler(404, 'User not found!'))
+		}
+		//password check
+		const validPassword = bcrypt.compareSync(password, validUser?.password)
+		if (!validPassword) {
+			return next(errorHandler(401, 'Wrong credentials!'))
+		}
+
+	} catch (error) {
+		next(error)
+	}
+}
