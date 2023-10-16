8c501f68577366ec6929cefa27835e0ab565ca65
nazmulrony <nhrony65@gmail.com>
2023-10-17 00:06:33 +0600

    sign up process complete

--- client/src/app/(auth)/sign-up/page.tsx
+++ client/src/app/(auth)/sign-up/page.tsx
@@ -1,23 +1,23 @@
 "use client";
+import Container from "@/components/Container";
 import { Button } from "@/components/ui/button";
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import {
     Form,
     FormControl,
-    FormDescription,
     FormField,
     FormItem,
     FormLabel,
     FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
-import Container from "@/components/Container";
-import Link from "next/link";
-import axios from "axios";
-import { error } from "console";
 import { Axios } from "@/lib/axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import * as z from "zod";
 
 const formSchema = z.object({
     username: z.string().min(2, {
@@ -30,6 +30,11 @@ const formSchema = z.object({
 });
 
 export default function SignUpPage() {
+    const router = useRouter();
+    //states
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+
     const form = useForm({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -40,6 +45,8 @@ export default function SignUpPage() {
     });
 
     const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+        setIsLoading(true);
+        setError(null);
         try {
             const res = await Axios.post("/auth/signup", data, {
                 headers: {
@@ -47,8 +54,12 @@ export default function SignUpPage() {
                 },
             });
             console.log("Signup successful:", res.data);
-        } catch (error) {
-            console.error("Signup failed:", error);
+            router.push("/sign-in");
+        } catch (error: any) {
+            console.error("Signup failed:", error?.response?.data?.message);
+            setError(error?.response?.data?.message);
+        } finally {
+            setIsLoading(false);
         }
     };
 
@@ -118,8 +129,17 @@ export default function SignUpPage() {
                                     </FormItem>
                                 )}
                             />
+                            {error && (
+                                <p className="text-sm text-red-500">{error}</p>
+                            )}
                             <Button type="submit" className="w-full">
-                                Submit
+                                {isLoading && (
+                                    <Loader2
+                                        color="white"
+                                        className="mr-2 h-4 w-4 animate-spin"
+                                    />
+                                )}
+                                Sign up
                             </Button>
                         </form>
                         <p className="mt-4">
